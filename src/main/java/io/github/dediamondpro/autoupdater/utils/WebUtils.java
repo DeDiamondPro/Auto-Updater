034c2a0d8fd18357d453e6101c6c9d5c2b698c82
DeDiamondPro <67508414+DeDiamondPro@users.noreply.github.com>
2021-10-06 18:06:43 +0200

    Update WebUtils.java

--- src/main/java/io/github/dediamondpro/autoupdater/utils/WebUtils.java
+++ src/main/java/io/github/dediamondpro/autoupdater/utils/WebUtils.java
@@ -5,7 +5,6 @@ import com.google.gson.JsonParser;
 
 import javax.net.ssl.HttpsURLConnection;
 import java.io.*;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.file.Files;
